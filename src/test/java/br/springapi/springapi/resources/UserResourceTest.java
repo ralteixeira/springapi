e5a4e545e881dc9f3b043ca99a9e02f79b4f9a09
ralteixeira <ralph.teixeira@mercadolivre.com>
2023-01-14 20:09:18 -0300

    teste para endpoint findAll implementado para classe user Resource

--- src/test/java/br/springapi/springapi/resources/UserResourceTest.java
+++ src/test/java/br/springapi/springapi/resources/UserResourceTest.java
@@ -4,24 +4,39 @@ import br.springapi.springapi.domain.User;
 import br.springapi.springapi.domain.dto.UserDTO;
 import br.springapi.springapi.services.UserService;
 import br.springapi.springapi.services.impl.UserServiceImpl;
+import org.h2.command.dml.MergeUsing;
+import org.h2.engine.DbObject;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
+import static org.h2.message.Trace.INDEX;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.when;
+
 @SpringBootTest
 class UserResourceTest {
 
-    private static final Integer ID = 1;
+    private static final Integer ID = 0;
     private static final String NAME = "Joao";
     private static final String EMAIL = "joao@email.com";
     private static final String PASSWORD = "1234";
+
+    private static final Integer INDEX = 0;
 
     
 
@@ -46,12 +61,44 @@ class UserResourceTest {
 
     }
 
-    @Test
-    void findById() {
+    @Test //GET ID userResource
+    void whenFindByIdThenReturnSuccess() {
+        when(service.findById(anyInt())).thenReturn(user);
+        when(mapper.map(any(), any())).thenReturn(userDTO);
+
+        ResponseEntity<UserDTO> response = resource.findById(ID);
+
+        assertNotNull(response);
+        assertNotNull(response.getBody());
+        assertEquals(ResponseEntity.class, response.getClass());
+        assertEquals(UserDTO.class, response.getBody().getClass());
+
+        assertEquals(ID, response.getBody().getId());
+        assertEquals(NAME, response.getBody().getName());
+        assertEquals(EMAIL, response.getBody().getEmail());
+        assertEquals(PASSWORD, response.getBody().getPassword());
     }
 
     @Test
-    void findAll() {
+    void whenFindAllThenReturnAnListOfUserDto() {
+        when(service.findAll()).thenReturn(List.of(user));
+        when(mapper.map(any(), any())).thenReturn(userDTO);
+
+        ResponseEntity<List<UserDTO>> response = resource.findAll();
+
+        assertNotNull(response);
+        assertNotNull(response.getBody());
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertEquals(ResponseEntity.class, response.getClass());
+        assertEquals(ArrayList.class, response.getBody().getClass());
+        assertEquals(UserDTO.class, response.getBody().get(INDEX).getClass());
+
+        assertEquals(ID, response.getBody().get(INDEX).getId());
+        assertEquals(NAME, response.getBody().get(INDEX).getName());
+        assertEquals(EMAIL, response.getBody().get(INDEX).getEmail());
+        assertEquals(PASSWORD, response.getBody().get(INDEX).getPassword());
+
+
     }
 
     @Test
